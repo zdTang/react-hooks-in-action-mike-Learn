7d4eab7392547f13c23f6b962484589adf6a242a
Mike Tang <Zhendongtt@gmail.com>
2022-12-27 19:15:03 -0500

    Display user's detail by tutorial

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import db from "../../static.json";
 const { users } = db;
 export default function UsersList() {
   const [userIndex, setUserIndex] = useState(0);
+  const user = users[userIndex];
 
   return (
-    <ul className="users items-list-nav">
-      {users.map((u, i) => (
-        <li key={u.id} className={i === userIndex ? "selected" : null}>
-          <button className="btn" onClick={() => setUserIndex(i)}>
-            {u.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <ul className="users items-list-nav">
+        {users.map((u, i) => (
+          <li key={u.id} className={i === userIndex ? "selected" : null}>
+            <button className="btn" onClick={() => setUserIndex(i)}>
+              {u.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="user-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
