06551ae87cf91943417bf6fe9b8d7131e80e8ccb
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-12-30 07:23:09 -0500

    use useRef to track uncontrolled component

--- src/components/Bookings/WeekPicker.js
+++ src/components/Bookings/WeekPicker.js
@@ -1,11 +1,24 @@
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 import reducer from "./weekReducer";
 import { getWeek } from "../../utils/date-wrangler";
-import { FaChevronLeft, FaCalendarDay, FaChevronRight } from "react-icons/fa";
+import {
+  FaChevronLeft,
+  FaCalendarDay,
+  FaChevronRight,
+  FaCalendarCheck,
+} from "react-icons/fa";
 
 export default function WeekPicker({ date }) {
   //Notice this useReducer has a lazy initialization. use a function and an initial argument
   const [week, dispatch] = useReducer(reducer, date, getWeek);
+  const textboxRef = useRef();
+
+  function goToDate() {
+    dispatch({
+      type: "SET_DATE",
+      payload: textboxRef.current.value,
+    });
+  }
   return (
     <div>
       <p className="date-picker">
@@ -17,6 +30,19 @@ export default function WeekPicker({ date }) {
           <FaCalendarDay />
           <span>Today</span>
         </button>
+        <span>
+          <input
+            type="text"
+            ref={textboxRef}
+            placeholder="e.g. 2020-09-02"
+            defaultValue="2023-01-01"
+          ></input>
+          <button onClick={goToDate}>
+            <FaCalendarCheck />
+            <span>Go</span>
+          </button>
+        </span>
+
         <button className="btn" onClick={() => dispatch({ type: "NEXT_WEEK" })}>
           <span>Next</span>
           <FaChevronRight />
